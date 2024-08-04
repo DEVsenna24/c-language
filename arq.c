1f9cce969a7cf7d99a83fcefe7ac8b252018f2a8
Davi Alves Sena <147349386+DEVsenna24@users.noreply.github.com>
2024-08-04 16:21:32 -0300

    Add files via upload

---
+++ arq.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    printf("oi tudo bem? tenho 18 anos e to programando em C.\n");
+
+    printf("valor inteiro: %i \n",10);
+    printf("valor decimal: %f \n",2.34653);
+    printf("valor decimal com duas casas decimais: %.2f \n", 2.34653);
+    printf("valor em caractere: %c \n", 'a');
+    printf("valor em caractere: %s \n", "abacate");
+}
